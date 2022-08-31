82b11326bdeb4d06ea62ed6e05d548c1210ad6f7
marvhus <44480334+marvhus@users.noreply.github.com>
2022-08-31 20:15:31 +0200

    Changed indentation

--- mine.pas
+++ mine.pas
@@ -13,114 +13,118 @@ type
       CursorCol: Integer;
    end;
 
-   function FieldGet(Field: Field; Row, Col: Integer): Cell;
-   begin
-      FieldGet := Field.Cells[Row*Field.Cols + Col];
-   end;
+function FieldIndexFromPosition(Field : Field; Row, Col : Integer ): Integer;
+begin
+   FielIndexFromPosition := Row*Field.Cols + Col;
+end;
 
-   function FieldIsOpen(Field: Field; Row, Col: Integer): Boolean;
-   begin
-      FieldIsOpen := Field.Open[Row*Field.Cols + Col];
-   end;
+function FieldGet(Field: Field; Row, Col: Integer): Cell;
+begin
+    FieldGet := Field.Cells[FieldIndexFromPosition(Field, Row, Col)];
+end;
 
-   procedure FieldOpenAtCursor(var Field: Field);
-   begin
-      Field.Open[Field.CursorRow*Field.Cols + Field.CursorCol] := True;
-   end;
+function FieldIsOpen(Field: Field; Row, Col: Integer): Boolean;
+begin
+    FieldIsOpen := Field.Open[FieldIndexFromPosition(Field, Row, Col)];
+end;
 
-   function FieldCheckedGet(Field: Field; Row, Col: Integer; var Cell: Cell): Boolean;
-   begin
-      FieldCheckedGet := (0 <= Row) and (Row < Field.Rows) and (0 <= Col) and (Col < Field.Cols);
-      if FieldCheckedGet then Cell := FieldGet(Field, Row, Col);
-   end;
+procedure FieldOpenAtCursor(var Field: Field);
+begin
+    Field.Open[FieldIndexFromPosition(Field, Row, Col)] := True;
+end;
 
-   procedure FieldSet(var Field: Field; Row, Col: Integer; Cell: Cell);
-   begin
-      Field.Cells[Row*Field.Cols + Col] := Cell;
-   end;
+function FieldCheckedGet(Field: Field; Row, Col: Integer; var Cell: Cell): Boolean;
+begin
+    FieldCheckedGet := (0 <= Row) and (Row < Field.Rows) and (0 <= Col) and (Col < Field.Cols);
+    if FieldCheckedGet then Cell := FieldGet(Field, Row, Col);
+end;
 
-   procedure FieldResize(var Field: Field; Rows, Cols: Integer);
-   begin
-      SetLength(Field.Cells, Rows*Cols);
-      SetLength(Field.Open, Rows*Cols);
-      Field.Rows := Rows;
-      Field.Cols := Cols;
-   end;
+procedure FieldSet(var Field: Field; Row, Col: Integer; Cell: Cell);
+begin
+    Field.Cells[FieldIndexFromPosition(Field, Row, Col)] := Cell;
+end;
 
-   function FieldRandomCell(Field: Field; var Row, Col: Integer): Cell;
-   begin
-      Row := Random(Field.Rows);
-      Col := Random(Field.Cols);
-      FieldRandomCell := FieldGet(Field, Row, Col);
-   end;
+procedure FieldResize(var Field: Field; Rows, Cols: Integer);
+begin
+    SetLength(Field.Cells, Rows*Cols);
+    SetLength(Field.Open, Rows*Cols);
+    Field.Rows := Rows;
+    Field.Cols := Cols;
+end;
 
-   procedure FieldRandomize(var Field: Field; BombsPercentage: Integer);
-   var
-      Index, BombsCount: Integer;
-      Row, Col: Integer;
-   begin
-      Field.CursorRow := 0;
-      Field.CursorCol := 0;
-      for Index := 0 to Field.Rows*Field.Cols do
-      begin
-         Field.Cells[Index] := Empty;
-         Field.Open[Index] := False;
-      end;
-      if BombsPercentage > 100 then BombsPercentage := 100;
-      BombsCount := (Field.Rows*Field.Cols*BombsPercentage + 99) div 100;
-      for Index := 1 to BombsCount do
-      begin
-         while FieldRandomCell(Field, Row, Col) = Bomb do;
-         FieldSet(Field, Row, Col, Bomb);
-      end;
-   end;
+function FieldRandomCell(Field: Field; var Row, Col: Integer): Cell;
+begin
+    Row := Random(Field.Rows);
+    Col := Random(Field.Cols);
+    FieldRandomCell := FieldGet(Field, Row, Col);
+end;
 
-   function FieldCountNbors(Field: Field; Row, Col: Integer): Integer;
-   var
-      DRow, DCol: Integer;
-      C: Cell;
-   begin
-      FieldCountNbors := 0;
-      for DRow := -1 to 1 do
-         for DCol := -1 to 1 do
-            if (DRow <> 0) or (DCol <> 0) then
-               if FieldCheckedGet(Field, Row + DRow, Col + DCol, C) then
-                  if C = Bomb then
-                     inc(FieldCountNbors);
-   end;
+procedure FieldRandomize(var Field: Field; BombsPercentage: Integer);
+var
+    Index, BombsCount: Integer;
+    Row, Col: Integer;
+begin
+    Field.CursorRow := 0;
+    Field.CursorCol := 0;
+    for Index := 0 to Field.Rows*Field.Cols do
+    begin
+        Field.Cells[Index] := Empty;
+        Field.Open[Index] := False;
+    end;
+    if BombsPercentage > 100 then BombsPercentage := 100;
+    BombsCount := (Field.Rows*Field.Cols*BombsPercentage + 99) div 100;
+    for Index := 1 to BombsCount do
+    begin
+        while FieldRandomCell(Field, Row, Col) = Bomb do;
+        FieldSet(Field, Row, Col, Bomb);
+    end;
+end;
 
-   function FieldAtCursor(Field: Field; Row, Col: Integer): Boolean;
-   begin
-      FieldAtCursor := (Field.CursorRow = Row) and (Field.CursorCol = Col);
-   end;
+function FieldCountNbors(Field: Field; Row, Col: Integer): Integer;
+var
+    DRow, DCol: Integer;
+    C: Cell;
+begin
+    FieldCountNbors := 0;
+    for DRow := -1 to 1 do
+        for DCol := -1 to 1 do
+        if (DRow <> 0) or (DCol <> 0) then
+            if FieldCheckedGet(Field, Row + DRow, Col + DCol, C) then
+                if C = Bomb then
+                    inc(FieldCountNbors);
+end;
 
-   procedure FieldWrite(Field: Field);
-   var
-      Row, Col, Nbors: Integer;
-   begin
-      for Row := 0 to Field.Rows-1 do
-      begin
-         for Col := 0 to Field.Cols-1 do
-         begin
-            if FieldAtCursor(Field, Row, Col) then Write('[') else Write(' ');
-            if FieldIsOpen(Field, Row, Col) then
-               case FieldGet(Field, Row, Col) of
-                  Bomb: Write('*');
-                  Empty: begin
-                            Nbors := FieldCountNbors(Field, Row, Col);
-                            if Nbors > 0 then Write(Nbors) else Write(' ');
-                         end;
-               end
+function FieldAtCursor(Field: Field; Row, Col: Integer): Boolean;
+begin
+    FieldAtCursor := (Field.CursorRow = Row) and (Field.CursorCol = Col);
+end;
+
+procedure FieldWrite(Field: Field);
+var
+    Row, Col, Nbors: Integer;
+begin
+    for Row := 0 to Field.Rows-1 do
+    begin
+        for Col := 0 to Field.Cols-1 do
+        begin
+           if FieldAtCursor(Field, Row, Col) then Write('[') else Write(' ');
+           if FieldIsOpen(Field, Row, Col) then
+              case FieldGet(Field, Row, Col) of
+                Bomb: Write('*');
+                Empty: begin
+                        Nbors := FieldCountNbors(Field, Row, Col);
+                        if Nbors > 0 then Write(Nbors) else Write(' ');
+                       end;
+              end
             else Write('#');
-            if FieldAtCursor(Field, Row, Col) then Write(']') else Write(' ');
-         end;
-         WriteLn
-      end
-   end;
+           if FieldAtCursor(Field, Row, Col) then Write(']') else Write(' ');
+        end;
+       WriteLn
+    end
+end;
 
 const
    STDIN_FILENO = 0;
-
 var
    MainField: Field;
    Quit: Boolean = False;
